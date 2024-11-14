94c4d4a8170025b09b943035f72319cde2db6e44
bpandey3 <bpandey3@hotmail.com>
2024-11-14 15:50:27 -0500

    aggrid with api

--- src/aggrid-example.js
+++ src/aggrid-example.js
@@ -1,12 +1,13 @@
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 // Theme
 import { ModuleRegistry } from '@ag-grid-community/core';
-import { AgGridReact } from '@ag-grid-community/react';
+import AgGridReact from '@ag-grid-community/react';
 // React Grid Logic
-import '@ag-grid-community/styles/ag-grid.css';
+// import  "ag-grid-community/styles/ag-grid.css";
 // Core CSS
-import '@ag-grid-community/styles/ag-theme-quartz.css';
+import "@ag-grid-community/styles";
 import React, { StrictMode, useEffect, useState } from 'react';
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 // ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
@@ -14,20 +15,20 @@ import React, { StrictMode, useEffect, useState } from 'react';
 export default function GridExample() {
     // Row Data: The data to be displayed.
     const [rowData, setRowData] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     // Column Definitions: Defines & controls grid columns.
-    const [colDefs, setColDefs] = useState([
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price' },
-        { field: 'electric' },
-    ]);
+    const [colDefs, setColDefs] = useState([]);
+    const columns =[        { field: 'userId' },
+        { field: 'id' },
+        { field: 'body' },
+        { field: 'title' }]
 
     const defaultColDef = {
         flex: 1,
     };
 
-    useEffect(()=>{
+    // useEffect(()=>{
 
         const setValues = [ { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
         { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
@@ -36,13 +37,36 @@ export default function GridExample() {
         { make: 'Fiat', model: '500', price: 15774, electric: false },
         { make: 'Nissan', model: 'Juke', price: 20675, electric: false },]
 
-        setInterval(()=>
-        {
-            setRowData(setValues);
-        })
+    //     setInterval(()=>
+    //     {
+    //         setRowData(setValues);
+    //     })
 
-    });
+    // });
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                console.log(data);
+                setColDefs(columns);
+                setRowData(data);
+                setLoading(false);
+
+    
+            } catch (error) { setError(error); setLoading(false); }
+
+        }
+
+        fetchData();
+    }, []);
+
+    if (loading) { return <div>Loading...</div>; } if (error) { return <div>Error: {error.message}</div>; }
     // Container: Defines the grid's theme & dimensions.
     return (
         <div
@@ -51,7 +75,7 @@ export default function GridExample() {
         }
         style={{ width: '100%', height: '100%' }}
         >
-            <AgGridReact rowData={rowData} columnDefs={colDefs} defaultColDef={defaultColDef} />
+            <AgGridReact rowData={rowData} columnDefs={colDefs}/>
         </div>
     );
 }
