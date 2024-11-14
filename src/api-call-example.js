741dce08e28764fb6e334fc8010e6fbac89e237c
codingindepth <63929620+codingindepth@users.noreply.github.com>
2024-11-13 22:59:43 -0500

    Update api-call-example.js

--- src/api-call-example.js
+++ src/api-call-example.js
@@ -1,23 +1,61 @@
 import axios from 'axios';
 import React from 'react';
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 export default function Apicall() {
     const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    //old code
+    // useEffect(() => {
+    //     axios.get(API_URL).then(resp =>
+    //         setPosts(resp.data)
+    //     ).catch(error => console.error(error));
+
+    // });
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then(resp =>
-            setPosts(resp.data)
-        ).catch(error => console.error(error));
+        const fetchData = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
 
-    });
+                const data = await response.json();
+                //console.log(data)
+                setPosts(data);
+
+                setLoading(false);
+            } catch (error) { setError(error); setLoading(false); }
+
+        }
+
+        fetchData();
+    }, []);
+
+    if (loading) { return <div>Loading...</div>; } if (error) { return <div>Error: {error.message}</div>; }
 
     return (
-        <ul>
-            {posts.map(post => (
-                <li key={post.id}>{post.title}</li>
-            ))}
+
+        // <div> <h1>API Data</h1> 
+        // {/* <pre>{JSON.stringify(data, null, 2)}</pre>  */}
+
+        // {/* <ul>
+        //     {posts.map(post => (
+        //         <li ID and Title is   key={post.id}>{post.title}</li>
+        //     ))}
+        // </ul> */}
+        // </div>
+
+        <div> <h1>API Data</h1> 
+        <ul> 
+            {posts.map((p, index) => 
+                (<li key={index}> User ID is: {p.id},   title : {p.title} </li>))}
         </ul>
+        </div>
     );
 
 }
